0d10d8f845946cf37be90bcc250e89512028d6a6
suthakarjs <suthakarjs22@gmail.com>
2022-07-16 12:57:00 +0530

    GitHub Demo

---
+++ src/com/github/GitHubDemo.java
@@ -0,0 +1,25 @@
+package com.github;
+
+public class GitHubDemo {
+
+	private void method1() {
+		System.out.println("From Method ~ 1");
+	}
+	
+	private void method2() {
+		System.out.println("From Method ~ 2");
+	}
+	
+	private void method3() {
+		System.out.println("From Method ~ 3");
+	}
+		
+	public static void main(String[] args) {
+		
+		GitHubDemo demo = new GitHubDemo();
+		demo.method1();
+		demo.method2();
+		demo.method3();	
+	}
+
+}
